e9a3241e34e44d60461da090979da1011ee239db
Thuan Le <thuanhieu159@gmail.com>
2019-10-26 11:29:51 -0700

    Test 1 Functions Added

---
+++ schedule-app/src/MainContent.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function MainContent(){
+	return (
+		<ul> 
+		<li> List 1: </li>
+		<li> List 2: </li>
+		<li> List 3: </li>
+		</ul>
+
+		)
+}
+
+export default MainContent;
