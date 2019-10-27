f1df024da39a5baf820ff70076031e8f2f2b59e9
Thuan Le <thuanhieu159@gmail.com>
2019-10-26 19:15:41 -0700

    divided into 4 sections

--- schedule-app/src/App.js
+++ schedule-app/src/App.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom"
 import Navbar from "./Components/Navbar"
 import MainContent from "./Components/MainContent"
 import Footer from "./Components/Footer"
+import barChanging from "./Components/barChanging"
+
 
 function App()
 {
   return (
     <div>
       <Navbar />
+      <barChanging />
       <MainContent />
       <Footer />
     </div>
---
+++ schedule-app/src/Components/barChanging.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+function barChanging() {
+	return (){
+	<h1> THIS IS OUR BAR OF WORK AND LIFE BALANCE</h1>
+	}
+}
+
+export default barChanging
